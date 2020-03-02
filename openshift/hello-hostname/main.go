983b80ced24e8a8b99e7ea01c21cd1ae538649ad
bonkstok <johnnyvanveen1991@gmail.com>
2020-03-02 19:30:11 +0100

    Create main.go

---
+++ openshift/hello-hostname/main.go
@@ -0,0 +1,21 @@
+// just a sample script to deploy on openshift with s2i
+// gets the container hostname and keep priting 
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	var containerHostname, err = os.Hostname()
+	if err != nil {
+		fmt.Printf("Failed to get hostname err: %s", err)
+		os.Exit(1)
+	}
+	for true {
+		fmt.Println("Hello from container", containerHostname)
+		time.Sleep(2 * time.Second)
+	}
+}
